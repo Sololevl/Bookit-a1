ad9783a5b400e5c4d391b345c9a1668518e0db61
Sololevl <130484906+Sololevl@users.noreply.github.com>
2025-10-31 22:34:30 +0530

    bug fix

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -9,11 +9,9 @@ dotenv.config();
 createTables()
   .then(() => {
     console.log('✅ Migration completed');
-    process.exit(0);
   })
   .catch((error) => {
     console.error('❌ Migration failed:', error);
-    process.exit(1);
   });
 
 const app: Application = express();
